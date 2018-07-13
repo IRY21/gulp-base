41e7d71ac05572621217f5c84fbcfc250ebe1b0d
IRY21 <rilok21work@gmail.com>
2018-07-13 12:51:44 +0500

    Update footer.php

--- src/template/all/footer.php
+++ src/template/all/footer.php
@@ -3,7 +3,7 @@
         <div class="Container">
             <div class="Footer-Body">
                 <a href="/im-blocks/politic.php" class="Footer-Politic ImPolitic fancybox.ajax">Политика конфиденциальности</a>
-                <a class="Footer-FijiLogo" href="http://fijiweb.ru/?utm_source=clientsite&utm_term=" target="_blank">
+                <a class="Footer-FijiLogo" href="http://fijiweb.ru/?utm_source=clientsite&utm_term=<?=$_SERVER['SERVER_NAME'];?>" target="_blank">
                     <img src="/img/LogoFiji.svg" alt="Создание сайтов Фидживеб" title="Создание сайтов Фидживеб">
                 </a>
             </div>
