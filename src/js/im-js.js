53c30211ec5cfbe2046e5c138073f7d308b2c9da
IRY21 <rilok21work@gmail.com>
2018-08-31 20:00:39 +0500

    fix

--- src/js/im-js.js
+++ src/js/im-js.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 
-    $(".masked-phone1").inputmask("+7 (999) 999-99-99",{
+    $(".masked-phone1").inputmask("8 (999) 999-99-99",{
         "oncomplete": function(){ phonemaskOk = 1; },
         "onincomplete": function(){ phonemaskOk = 0; },
         "oncleared": function(){ phonemaskOk = 0; }
     });
 
-    $(".ProcessingPersonalDataText").fancybox({
+    $(".ProcessingPersonalDataText-Link").fancybox({
         maxWidth : 700
     });
 
