e33346ffbfd526ab6fe08dc5d56d19436e809ee5
IRY21 <rilok21work@gmail.com>
2018-11-28 15:48:31 +0500

    font-async

--- gulp/async-font.js
+++ gulp/async-font.js
@@ -7,20 +7,14 @@ babel                       = require('gulp-babel');
 include                     = require("gulp-include");
 uglify                      = require('gulp-uglify');
 
-gulp.task('js', function () {
-    return gulp.src(cnf.src.js)
+gulp.task('asyncFonts', function () {
+    return gulp.src(cnf.src.asyncFonts)
         .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-        .pipe(sourcemaps.init({loadMaps: true}))
         .pipe(babel())
-        .pipe(include({
-            extensions: "js",
-            hardFail: true
-        }))
-        //.pipe(uglify())
-        .pipe(sourcemaps.write('.'))
+        .pipe(uglify())
         .pipe(gulp.dest(cnf.dist.js));
 });
 
-gulp.task('js:watch', function () {
-    gulp.watch([cnf.src.js, 'src/js/components/**/*.js'], ['js', 'bs-reload']);
+gulp.task('asyncFonts:watch', function () {
+    gulp.watch(cnf.src.asyncFonts, ['asyncFonts', 'bs-reload']);
 });
